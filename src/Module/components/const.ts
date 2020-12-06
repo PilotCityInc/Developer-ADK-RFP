3834eab4666e982893b66431c5d709aa8b330161
Prajit Saravanan <prajitsaravanan@Prajits-MacBook-Pro.local>
2020-12-06 03:42:06 -0800

    💄 added the drop down menu options

---
+++ src/Module/components/const.ts
@@ -0,0 +1,38 @@
+export const outcomes = ['Build portfolio project', 'Qualify for internship to execute on project'];
+export const outcomesValue = [
+  'Build portfolio project',
+  'Qualify for internship to execute on project'
+];
+export const deliverables = [
+  'Business Model Canvas',
+  'One Sentence Pitch',
+  'Elevator Pitch',
+  'Design & Prototype Log',
+  'Prototype Demo',
+  'Presentation Deck'
+];
+export const deliverablesValue = [
+  'Business Model Canvas',
+  'One Sentence Pitch',
+  'Elevator Pitch',
+  'Design & Prototype Log',
+  'Prototype Demo',
+  'Presentation Deck'
+];
+export const chips = ['Must use employer product'];
+export const items = [
+  'Must use employer product',
+  'Must use employer service',
+  'Must use employer process'
+];
+
+export const group = ['Setup', 'Project', 'Screening', 'Internship'];
+export const required = ['Creator requires this activity', 'Yes', 'No'];
+export const deliverable = ['Yes', 'No'];
+
+export const endEarly = [
+  'Creator has not allowed participants to end early after this activity',
+  'Creator has allow end early option only at preset order placement',
+  'Yes',
+  'No'
+];
