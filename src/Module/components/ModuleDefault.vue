ae6b8d1c270b582d420d33c70302ae9cf906ebf7
Derick Lee <dericklee@pilotcity.com>
2021-02-24 22:21:56 -0800

    version 1.0.26

--- src/Module/components/ModuleDefault.vue
+++ src/Module/components/ModuleDefault.vue
@@ -46,7 +46,7 @@
       <!-- ENTER CONTENT HERE -->
       <!-- DESIGN YOUR ACTIVITY HERE / COMMENT OUT WHEN YOU'VE STARTED DESIGNING -->
       <!-- <div class="module-default__none">Design your activity here</div> -->
-      <div class="module-default__scope">
+      <div class="module-default__scope mt-8">
         <v-btn
           dense
           color="blue"
